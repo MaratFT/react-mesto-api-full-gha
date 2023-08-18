dfed6a77e708821cbe6e5cf9baa7fff39174e437
Marat Mustafin <mustafin.m-marat@yandex.ru>
2023-08-18 21:15:45 +0400

    sprint 15 check push3

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ const allowedCors = [
   'https://maratft007.nomoreparties.co',
 ];
 
-const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
